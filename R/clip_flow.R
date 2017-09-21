30527a18d8e1b1064dcaa63109d168509d9e3c12
Jarad Niemi <niemi@iastate.edu>
2017-09-21 10:43:59 -0500

    Stash changes for impending merge

--- R/clip_flow.R
+++ R/clip_flow.R
@@ -3,15 +3,14 @@
 #' The flumes that measure flow sometimes get stuck. This is clear when there
 #' has been no rainfall recently, but the flume is still recording flow. When
 #' this occurs, we set the flow to zero. This function sorts the
-#' \code{data.frame} by date_time and then calculates the rain in the previous
-#' 24*12 rows of the data.frame as this assumes the data are in 5 minute
-#' increments.
+#' \code{data.frame} by date_time and then calculates the max rain in the
+#' previous \code{window} rows of the data.frame (ignoring NAs).
+#' If this max is zero, then flow is set to zero.
 #'
 #' @param data A \code{data.frame} containing columns \code{date_time},
 #'   \code{flow}, and \code{rain}.
-#' @param window An integer (default is 24*12, i.e. the number of 5-minute
-#' intervals in 24 hours) indicating the number of observations in the
-#' window.
+#' @param window An integer indicating the number of minutes to calculate the
+#'   max rain over. Default is 24*60, the number of minutes in a day.
 #' @return A \code{data.frame} arranged by \code{date_time} with the flow column
 #'   updated so that flow is 0 when no rain has fallen.
 #' @import dplyr
@@ -19,25 +18,37 @@
 #' @importFrom zoo rollapply
 #' @export
 clip_flow <- function(data,
-                      window = 24 * 12) {  # number of  5-minute intervals in 24 hours
+                      window = 24*60) {
   stopifnot("flow" %in% names(data),
             "date_time" %in% names(data),
             "rain" %in% names(data))
 
+  # Construct a data.frame with every minute
+  d_full <- data.frame(date_time = seq(min(data$date_time),
+                                       max(data$date_time),
+                                       by = "1 min"))
+
+  # Expand data set to include every minute
   d <- data %>%
-    dplyr::arrange(date_time) %>%
+    dplyr::mutate(original = TRUE) %>%
+    dplyr::right_join(d_full, by = "date_time")  %>%
 
     # Calculate rain in last 24 hours
     dplyr::ungroup() %>%
-    dplyr::mutate(rain_last24 = zoo::rollapply(rain,
-                                               window,
-                                               max,
-                                               partial = TRUE,
-                                               align = "right")) %>%
+    dplyr::mutate(rain_max = zoo::rollapply(rain,
+                                            window,
+                                            max,
+                                            partial = TRUE,
+                                            align = "right")) %>%
 
     # Set flow to 0 if no rain in last 24 hours
-    dplyr::mutate(flow = ifelse(rain_last24 > 0, flow, 0)) %>%
+    dplyr::mutate(flow = ifelse(rain_max > 0 | is.na(rain_max), flow, 0)) %>%
 
-    # Remove unnecessary columns
-    dplyr::select(-rain_last24)
+    # Remove unnecessary rows and columns
+    dplyr::filter(original) %>%
+    dplyr::select(-rain_max, -original)
+
+  stopifnot(all(dim(data) == dim(d)))
+
+  return(d)
 }
